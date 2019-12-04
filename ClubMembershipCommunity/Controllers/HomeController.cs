d4b21816ad0ed38a705524ea9159127246d8a5fe
Earl Francis <lefrancis@gmail.com>
2019-12-04 11:06:45 -0600

    12/04/2019 11:06 AM

--- ClubMembershipCommunity/Controllers/HomeController.cs
+++ ClubMembershipCommunity/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace ClubMembershipCommunity.Controllers
 {
@@ -10,7 +11,7 @@ namespace ClubMembershipCommunity.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Title = ConfigurationManager.AppSettings["AppName"].ToString();
 
             return View();
         }
